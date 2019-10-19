df4970e8569e9045f6b53d45ff375f4459d906bd
Cameron Brill <c00brill@gmail.com>
2019-10-19 17:01:25 -0500

    Linted stuff.

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def root_route():
     return "Hello, World!"
 
+
 @app.route('/restaurantsWithinZip')
-def restaurants_within_zip( zip_code : int ) -> dict: 
+def restaurants_within_zip(zip_code: int) -> dict: 
     x = {}
     return x
 
+
 @app.route('/restaurantsWithinVicinity')
-def restaurants_within_vicinity( coords:tuple ) -> dict:
+def restaurants_within_vicinity(coords: tuple) -> dict:
     zip = coords[0]
     restaurants = restaurants_within_zip(zip)
     return restaurants
 
+
 @app.route('/getPopularIngredients')
-def get_popular_ingredients( coords:tuple ) -> list:
-    l = []
+def get_popular_ingredients(coords: tuple) -> list:
+    ingredients = []
     restaurants = restaurants_within_vicinity(coords)
     for i in restaurants:
-        l.append(i[0])
-    return l
-
+        ingredients.append(i[0])
+    return ingredients
