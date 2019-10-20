b00aed759dddb6b7442e32ccc364105a03f800dc
Cameron Brill <c00brill@gmail.com>
2019-10-20 04:18:58 -0500

    Finished getByIngredientAndCuisine route.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data_path = './../dataset/clean/just_tacos_and_burritos_v2.csv' 
 data = pd.read_csv(data_path)
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
@@ -10,11 +10,26 @@ def root_route():
     return data.head().to_json()
 
 
-@app.route('/restaurantsWithinZip')
-def restaurants_within_zip(zip_code: int) -> dict: 
-    x = {}
-    print(data.head())
-    return x
+@app.route('/restaurantsWithinZip', methods=['POST'])
+def restaurants_within_zip(zip_code: str = "") -> dict: 
+    req = request.get_json()
+    zip_code = req['zip_code']
+    return data[data['postalcode']==zip_code]
+
+
+@app.route('/getByIngredientAndCuisine', methods=['POST'])
+def get_by_ingredient_and_cuisine(ingredients: list = [], cuisine: list = []) -> dict:
+    req = request.get_json()
+    ingredients = req["ingredients"]
+    cuisine = req["cuisine"]
+    filtered_data = data[:]
+    if len(ingredients) > 0:
+        ingr_pattern = '|'.join([f'(?i){x}' for x in ingredients])
+        filtered_data = filtered_data[filtered_data['menus.description'].str.contains(ingr_pattern, na=False, regex=True)]
+    if len(cuisine) > 0:
+        cuis_pattern = '|'.join([f'(?i){x}' for x in cuisine])
+        filtered_data = filtered_data[filtered_data['cuisines'].str.contains(cuis_pattern, na=False, regex=True)]
+    return filtered_data.reset_index().to_json(orient='records')
 
 
 @app.route('/restaurantsWithinVicinity')
@@ -31,3 +46,4 @@ def get_popular_ingredients(coords: tuple) -> list:
     for i in restaurants:
         ingredients.append(i[0])
     return ingredients
+
